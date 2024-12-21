998fcc489adf5137f06b3a821b9ca6c3d6233f25
osimhi213 <ofeksim1@gmail.com>
2024-12-21 14:39:01 +0200

    include outputs in the repo

--- Ex2/main.py
+++ Ex2/main.py
@@ -6,10 +6,6 @@ import matplotlib.image as mpimg
 from solution import Solution
 
 
-COST1 = 0.5
-COST2 = 3.0
-WIN_SIZE = 3
-DISPARITY_RANGE = 30
 ##########################################################
 # Don't forget to fill in your IDs!!!
 # students' IDs:
@@ -26,8 +22,8 @@ def toc(t):
     return float(tic()) - float(t)
 
 
-def forward_map(left_image, labels):
-    labels -= DISPARITY_RANGE
+def forward_map(left_image, labels, disparity_range):
+    labels -= disparity_range
     mapped = np.zeros_like(left_image)
     for row in range(left_image.shape[0]):
         cols = range(left_image.shape[1])
@@ -49,157 +45,161 @@ def load_data(is_your_data=False):
 
 
 def main():
-    # left_image, right_image = load_data()
-    # solution = Solution()
-    # # Compute Sum-Square-Diff distance
-    # tt = tic()
-    # ssdd = solution.ssd_distance(left_image.astype(np.float64),
-    #                              right_image.astype(np.float64),
-    #                              win_size=WIN_SIZE,
-    #                              dsp_range=DISPARITY_RANGE)
-    # print(f"SSDD calculation done in {toc(tt):.4f}[seconds]")
+    COST1 = 0.5
+    COST2 = 3.0
+    WIN_SIZE = 3
+    DISPARITY_RANGE = 20
+    left_image, right_image = load_data()
+    solution = Solution()
+    # Compute Sum-Square-Diff distance
+    tt = tic()
+    ssdd = solution.ssd_distance(left_image.astype(np.float64),
+                                 right_image.astype(np.float64),
+                                 win_size=WIN_SIZE,
+                                 dsp_range=DISPARITY_RANGE)
+    print(f"SSDD calculation done in {toc(tt):.4f}[seconds]")
 
-    # # Construct naive disparity image
-    # tt = tic()
-    # label_map = solution.naive_labeling(ssdd)
-    # print(f"Naive labeling done in {toc(tt):.4f}[seconds]")
+    # Construct naive disparity image
+    tt = tic()
+    label_map = solution.naive_labeling(ssdd)
+    print(f"Naive labeling done in {toc(tt):.4f}[seconds]")
 
-    # # plot the left image and the estimated depth
-    # fig = plt.figure()
-    # plt.subplot(1, 2, 1)
-    # plt.imshow(left_image)
-    # plt.title('Source Image')
-    # plt.subplot(1, 2, 2)
-    # plt.imshow(label_map)
-    # plt.colorbar()
-    # plt.title('Naive Depth')
-    # plt.savefig('outputs/naive_labeling.jpg')
+    # plot the left image and the estimated depth
+    fig = plt.figure()
+    plt.subplot(1, 2, 1)
+    plt.imshow(left_image)
+    plt.title('Source Image')
+    plt.subplot(1, 2, 2)
+    plt.imshow(label_map)
+    plt.colorbar()
+    plt.title('Naive Depth')
+    plt.savefig('outputs/naive_labeling.jpg')
 
-    # # Compute forward map of the left image to the right image.
-    # mapped_image_no_smooth_naive = forward_map(left_image, labels=label_map)
-    # # plot left image, forward map image and right image
-    # plt.figure()
-    # plt.subplot(1, 3, 1)
-    # plt.imshow(left_image)
-    # plt.title('Source Image')
-    # plt.subplot(1, 3, 2)
-    # plt.imshow(mapped_image_no_smooth_naive)
-    # plt.title('Forward map - Naive')
-    # plt.subplot(1, 3, 3)
-    # plt.imshow(right_image)
-    # plt.title('Right Image')
-    # plt.savefig('outputs/naive_labeling_map.jpg')
+    # Compute forward map of the left image to the right image.
+    mapped_image_no_smooth_naive = forward_map(left_image, labels=label_map, disparity_range=DISPARITY_RANGE)
+    # plot left image, forward map image and right image
+    plt.figure()
+    plt.subplot(1, 3, 1)
+    plt.imshow(left_image)
+    plt.title('Source Image')
+    plt.subplot(1, 3, 2)
+    plt.imshow(mapped_image_no_smooth_naive)
+    plt.title('Forward map - Naive')
+    plt.subplot(1, 3, 3)
+    plt.imshow(right_image)
+    plt.title('Right Image')
+    plt.savefig('outputs/naive_labeling_map.jpg')
 
-    # # Smooth disparity image - Dynamic Programming
-    # tt = tic()
-    # label_smooth_dp = solution.dp_labeling(ssdd, COST1, COST2)
-    # print(f"Dynamic Programming done in {toc(tt):.4f}[seconds]")
+    # Smooth disparity image - Dynamic Programming
+    tt = tic()
+    label_smooth_dp = solution.dp_labeling(ssdd, COST1, COST2)
+    print(f"Dynamic Programming done in {toc(tt):.4f}[seconds]")
 
-    # # plot the left image and the estimated depth
-    # plt.figure()
-    # plt.subplot(1, 2, 1)
-    # plt.imshow(left_image)
-    # plt.title('Source Image')
-    # plt.subplot(1, 2, 2)
-    # plt.imshow(label_smooth_dp)
-    # plt.colorbar()
-    # plt.title('Smooth Depth - DP')
-    # plt.savefig('outputs/dp_labeling.jpg')
+    # plot the left image and the estimated depth
+    plt.figure()
+    plt.subplot(1, 2, 1)
+    plt.imshow(left_image)
+    plt.title('Source Image')
+    plt.subplot(1, 2, 2)
+    plt.imshow(label_smooth_dp)
+    plt.colorbar()
+    plt.title('Smooth Depth - DP')
+    plt.savefig('outputs/dp_labeling.jpg')
 
-    # # Compute forward map of the left image to the right image.
-    # mapped_image_smooth_dp = forward_map(left_image, labels=label_smooth_dp)
-    # # plot left image, forward map image and right image
-    # plt.figure()
-    # plt.subplot(1, 3, 1)
-    # plt.imshow(left_image)
-    # plt.title('Source Image')
-    # plt.subplot(1, 3, 2)
-    # plt.imshow(mapped_image_smooth_dp)
-    # plt.title('Smooth Forward map - DP')
-    # plt.subplot(1, 3, 3)
-    # plt.imshow(right_image)
-    # plt.title('Right Image')
-    # plt.savefig('outputs/dp_labeling_map.jpg')
+    # Compute forward map of the left image to the right image.
+    mapped_image_smooth_dp = forward_map(left_image, labels=label_smooth_dp, disparity_range=DISPARITY_RANGE)
+    # plot left image, forward map image and right image
+    plt.figure()
+    plt.subplot(1, 3, 1)
+    plt.imshow(left_image)
+    plt.title('Source Image')
+    plt.subplot(1, 3, 2)
+    plt.imshow(mapped_image_smooth_dp)
+    plt.title('Smooth Forward map - DP')
+    plt.subplot(1, 3, 3)
+    plt.imshow(right_image)
+    plt.title('Right Image')
+    plt.savefig('outputs/dp_labeling_map.jpg')
 
-    # # plot the left image and the estimated depth
-    # plt.figure()
-    # plt.subplot(1, 2, 1)
-    # plt.imshow(label_map)
-    # plt.title('Naive Depth')
-    # plt.subplot(1, 2, 2)
-    # plt.imshow(label_smooth_dp)
-    # plt.title('Smooth Depth - DP')
-    # plt.savefig('outputs/depth_compare_dp.jpg')
+    # plot the left image and the estimated depth
+    plt.figure()
+    plt.subplot(1, 2, 1)
+    plt.imshow(label_map)
+    plt.title('Naive Depth')
+    plt.subplot(1, 2, 2)
+    plt.imshow(label_smooth_dp)
+    plt.title('Smooth Depth - DP')
+    plt.savefig('outputs/depth_compare_dp.jpg')
 
-    # # Generate a dictionary which maps each direction to a label map:
-    # tt = tic()
-    # direction_to_vote = solution.dp_labeling_per_direction(ssdd, COST1, COST2)
-    # print(f"Dynamic programming in all directions done in {toc(tt):.4f}"
-    #       f"[seconds]")
+    # Generate a dictionary which maps each direction to a label map:
+    tt = tic()
+    direction_to_vote = solution.dp_labeling_per_direction(ssdd, COST1, COST2)
+    print(f"Dynamic programming in all directions done in {toc(tt):.4f}"
+          f"[seconds]")
 
-    # # Plot all directions as well as the image, in the center of the plot:
-    # plt.figure()
-    # for i in range(1, 1 + 9):
-    #     plt.subplot(3, 3, i)
-    #     if i < 5:
-    #         plt.imshow(direction_to_vote[i])
-    #         plt.title(f'Direction {i}')
-    #     elif i == 5:
-    #         plt.imshow(left_image)
-    #         plt.title(f'Left Image')
-    #     else:
-    #         plt.imshow(direction_to_vote[i - 1])
-    #         plt.title(f'Direction {i - 1}')
+    # Plot all directions as well as the image, in the center of the plot:
+    plt.figure()
+    for i in range(1, 1 + 9):
+        plt.subplot(3, 3, i)
+        if i < 5:
+            plt.imshow(direction_to_vote[i])
+            plt.title(f'Direction {i}')
+        elif i == 5:
+            plt.imshow(left_image)
+            plt.title(f'Left Image')
+        else:
+            plt.imshow(direction_to_vote[i - 1])
+            plt.title(f'Direction {i - 1}')
 
-    # plt.savefig('outputs/dp_labeling_per_direction.jpg')
+    plt.savefig('outputs/dp_labeling_per_direction.jpg')
 
-    # # Smooth disparity image - Semi-Global Mapping
-    # tt = tic()
-    # label_smooth_sgm = solution.sgm_labeling(ssdd, COST1, COST2)
-    # print(f"SGM done in {toc(tt):.4f}[seconds]")
+    # Smooth disparity image - Semi-Global Mapping
+    tt = tic()
+    label_smooth_sgm = solution.sgm_labeling(ssdd, COST1, COST2)
+    print(f"SGM done in {toc(tt):.4f}[seconds]")
 
-    # # Plot Semi-Global Mapping result:
-    # plt.figure()
-    # plt.subplot(1, 2, 1)
-    # plt.imshow(left_image)
-    # plt.title('Source Image')
-    # plt.subplot(1, 2, 2)
-    # plt.imshow(label_smooth_sgm)
-    # plt.colorbar()
-    # plt.title('Smooth Depth - SGM')
-    # plt.savefig('outputs/sgm_labeling.jpg')
+    # Plot Semi-Global Mapping result:
+    plt.figure()
+    plt.subplot(1, 2, 1)
+    plt.imshow(left_image)
+    plt.title('Source Image')
+    plt.subplot(1, 2, 2)
+    plt.imshow(label_smooth_sgm)
+    plt.colorbar()
+    plt.title('Smooth Depth - SGM')
+    plt.savefig('outputs/sgm_labeling.jpg')
 
-    # # Plot the forward map based on the Semi-Global Mapping result:
-    # mapped_image_smooth_sgm = forward_map(left_image, labels=label_smooth_sgm)
-    # plt.figure()
-    # plt.subplot(1, 3, 1)
-    # plt.imshow(left_image)
-    # plt.title('Source Image')
-    # plt.subplot(1, 3, 2)
-    # plt.imshow(mapped_image_smooth_sgm)
-    # plt.title('Smooth Forward map - SGM')
-    # plt.subplot(1, 3, 3)
-    # plt.imshow(right_image)
-    # plt.title('Right Image')
-    # plt.savefig('outputs/sgm_labeling_map.jpg')
+    # Plot the forward map based on the Semi-Global Mapping result:
+    mapped_image_smooth_sgm = forward_map(left_image, labels=label_smooth_sgm, disparity_range=DISPARITY_RANGE)
+    plt.figure()
+    plt.subplot(1, 3, 1)
+    plt.imshow(left_image)
+    plt.title('Source Image')
+    plt.subplot(1, 3, 2)
+    plt.imshow(mapped_image_smooth_sgm)
+    plt.title('Smooth Forward map - SGM')
+    plt.subplot(1, 3, 3)
+    plt.imshow(right_image)
+    plt.title('Right Image')
+    plt.savefig('outputs/sgm_labeling_map.jpg')
 
-    # # plot the left image and the estimated depth
-    # plt.figure()
-    # plt.subplot(1, 2, 1)
-    # plt.imshow(label_map)
-    # plt.title('Naive Depth')
-    # plt.subplot(1, 2, 2)
-    # plt.imshow(mapped_image_smooth_sgm)
-    # plt.title('Smooth Depth - SGM')
-    # plt.savefig('outputs/depth_compare_sgm.jpg')
+    # plot the left image and the estimated depth
+    plt.figure()
+    plt.subplot(1, 2, 1)
+    plt.imshow(label_map)
+    plt.title('Naive Depth')
+    plt.subplot(1, 2, 2)
+    plt.imshow(mapped_image_smooth_sgm)
+    plt.title('Smooth Depth - SGM')
+    plt.savefig('outputs/depth_compare_sgm.jpg')
 
     ###########################################################################
     ########################### YOUR IMAGE PLAYGROUND #########################
     ###########################################################################
-    # COST1 = 0.5           # YOU MAY CHANGE THIS
-    # COST2 = 3.0           # YOU MAY CHANGE THIS
-    # WIN_SIZE = 3          # YOU MAY CHANGE THIS
-    # DISPARITY_RANGE = 20  # YOU MAY CHANGE THIS
+    COST1 = 0.5           # YOU MAY CHANGE THIS
+    COST2 = 3.0           # YOU MAY CHANGE THIS
+    WIN_SIZE = 3          # YOU MAY CHANGE THIS
+    DISPARITY_RANGE = 30  # YOU MAY CHANGE THIS
 
     your_left_image, your_right_image = load_data(is_your_data=True)
     print(your_left_image.shape, your_right_image.shape)
@@ -250,7 +250,8 @@ def main():
 
     # Plot the forward map based on the Semi-Global Mapping result:
     your_mapped_image_smooth_sgm = forward_map(your_left_image,
-                                               labels=your_label_smooth_sgm)
+                                               labels=your_label_smooth_sgm,
+                                               disparity_range=DISPARITY_RANGE)
     plt.figure()
     plt.subplot(1, 3, 1)
     plt.imshow(your_left_image)
